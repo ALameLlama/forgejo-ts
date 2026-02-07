2fd21c3d85fbf6eedd537a12dbb2aa8109e9f166
Nicholas Ciechanowski <nicholas@ciech.anow.ski>
2026-02-08 00:42:08 +1100

    feat: swap to `hey-api/openapi-ts`

--- src/generated/models/BlockedUser.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type BlockedUser = {
-    block_id?: number;
-    created_at?: string;
-};
-
